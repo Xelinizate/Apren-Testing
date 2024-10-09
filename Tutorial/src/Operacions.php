9305d2c7c1c43c3e5038b50b544d7120957d78b2
Adriana CP <xelinizate86@gmail.com>
2024-10-09 14:00:18 +0200

    Prueba proyecto

---
+++ Tutorial/src/Operacions.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Operations{
+
+        public function __construct()
+        {
+            
+        }
+
+        public function sum($n1, $n2){
+            return $n1 + $n2;
+        }
+
+        public function division($n1, $n2){
+            return $n1 / $n2;
+        }
+    }
+
+
+
+?>
